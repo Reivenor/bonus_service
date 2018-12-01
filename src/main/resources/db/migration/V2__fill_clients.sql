b60a4809f87dfbbd6cf474c99afd553cf76d2a3a
IBrusentsev <Ibrusentsev@spb.vimpelcom.ru>
2018-12-01 17:24:27 +0300

    delete migrations

--- src/main/resources/db/migration/V2__fill_clients.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO client(cardId, balance) VALUES (11111111, 10);
-INSERT INTO client(cardId, balance) VALUES (22222222, 0);
-INSERT INTO client(cardId, balance) VALUES (33333333, 100);
-INSERT INTO client(cardId, balance) VALUES (44444444, 0);
-INSERT INTO client(cardId, balance) VALUES (55555555, 0);
-INSERT INTO client(cardId, balance) VALUES (66666666, 100000);
