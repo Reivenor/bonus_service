dbee748026f05469790b4feacc0f8928146c4a7a
IBrusentsev <Ibrusentsev@spb.vimpelcom.ru>
2018-12-02 13:19:26 +0300

    dispathcer implementation

--- dispatcher/src/main/java/ru/phil_it/tender/dev_2/web/DispatcherController.java
+++ dispatcher/src/main/java/ru/phil_it/tender/dev_2/web/DispatcherController.java
@@ -1,5 +1,6 @@
 package ru.phil_it.tender.dev_2.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +12,12 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import ru.phil_it.tender.dev_2.domain.dto.NewBill;
-import ru.phil_it.tender.dev_2.service.impl.exceptions.CardNumberNotFound;
 
 @SpringBootApplication
 @RestController
-@RibbonClient(name = "say-hello", configuration = DispatcherControllerConfiguration.class)
+@Slf4j
+@RequestMapping("/bonus")
+@RibbonClient(name = "bonus", configuration = DispatcherControllerConfiguration.class)
 public class DispatcherController {
     @LoadBalanced
     @Bean
@@ -28,9 +30,16 @@ public class DispatcherController {
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> correctBonusPoints(@RequestBody NewBill newBill)
-            throws CardNumberNotFound {
-        //todo not work
-        return this.restTemplate.getForObject("http://say-hello/greeting", ResponseEntity.class);
+    public ResponseEntity<Object> correctBonusPoints(@RequestBody NewBill newBill) {
+        log.info("New bill");
+        //todo надо тестить работает ли
+        return this.restTemplate.postForEntity("http://bonus/bonus", newBill, Object.class);
     }
+
+    @GetMapping(value = "/{cardId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> getBonusBalance(@PathVariable("cardId") Integer cardId) {
+        log.info("Asked balance for card " + cardId);
+        return this.restTemplate.getForEntity("http://bonus/bonus/" + cardId, Object.class);
+    }
+
 }
